327345921d99d190485cacc2449a85867587e680
YokoMentor <91747328+YokoMentor@users.noreply.github.com>
2026-05-18 11:07:09 +0300

    Created dark bg

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center">
-      Hello
+    <div className={`${styles.bg_dark} flex flex-col min-h-screen justify-center items-center bg-bg-dark bg-no-repeat bg-contain`}>
+      <div>Hello</div>
     </div>
   );
 }
