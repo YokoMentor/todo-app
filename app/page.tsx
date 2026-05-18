c8bfb180001d55fc2ce82cad26aba1c3fc4123ea
YokoMentor <91747328+YokoMentor@users.noreply.github.com>
2026-05-18 22:23:15 +0300

    Theme switcher's initial functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,34 @@
 'use client'
+import React, { useState } from 'react';
 import styles from './page.module.css';
 
 export default function Home() {
+
+    const [darkBg, setDarkBg] = useState(true);
+    const [lightBg, setLightBg] = useState(false); 
+    const [themeSwitch, setThemeSwitch] = useState(true);
+    
+    function handleThemeSwitcher(event: ToggleEvent) {
+    event.preventDefault();
+    if (themeSwitch) {
+      setDarkBg(true);
+      setLightBg(false);
+    } else {
+      setDarkBg(false);
+      setLightBg(true);
+    }
+    setThemeSwitch(!themeSwitch);
+  }
+
   return (
     <div className={`${styles.bg_dark} flex flex-col min-h-screen justify-center items-center bg-bg-dark bg-no-repeat bg-contain`}>
-      <div>Hello</div>
+      <div className='flex flex-col justify-center items-center w-[328px]'>
+        <div className='flex justify-between items-baseline w-full'>
+          <h1 className='font-bold text-[28px] text-white tracking-[.32em]'>TODO</h1>
+          {darkBg && (<div className={`${styles.icon_moon} w-[20px] h-[20px] bg-no-repeat bg-center bg-contain`} onClick={handleThemeSwitcher}></div>)}
+          {lightBg && (<div className={`${styles.icon_sun} w-[20px] h-[20px] bg-no-repeat bg-center bg-contain`} onClick={handleThemeSwitcher}></div>)}  
+        </div>
+      </div>
     </div>
   );
 }
