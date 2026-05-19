9dcd3849c000ff4f2b7fde0e04b346e2bb7c4a7e
YokoMentor <91747328+YokoMentor@users.noreply.github.com>
2026-05-19 09:21:55 +0300

    Removed redundant code from theme switcher

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,29 @@
 'use client'
-import React, { useState } from 'react';
+import { useState } from 'react';
 import styles from './page.module.css';
 
 export default function Home() {
+    const darkBgStyle = `${styles.bg_dark} flex flex-col min-h-screen justify-center items-center bg-bg-dark bg-no-repeat bg-contain`;
+    const lightBgStyle = `${styles.bg_light} flex flex-col min-h-screen justify-center items-center bg-bg-light bg-no-repeat bg-contain`;
 
     const [darkBg, setDarkBg] = useState(true);
-    const [lightBg, setLightBg] = useState(false); 
     const [themeSwitch, setThemeSwitch] = useState(true);
     
     function handleThemeSwitcher() {
     if (themeSwitch) {
       setDarkBg(true);
-      setLightBg(false);
     } else {
       setDarkBg(false);
-      setLightBg(true);
     }
     setThemeSwitch(!themeSwitch);
   }
 
   return (
-    <div className={`${darkBg ? styles.bg_dark : styles.bg_light} flex flex-col min-h-screen justify-center items-center bg-bg-dark bg-no-repeat bg-contain`}>
+    <div className={`${darkBg ? darkBgStyle : lightBgStyle}`}>
       <div className='flex flex-col justify-center items-center w-[328px]'>
         <div className='flex justify-between items-baseline w-full'>
           <h1 className='font-bold text-[28px] text-white tracking-[.32em]'>TODO</h1>
-          {darkBg && (<button className={`${styles.icon_moon} w-[20px] h-[20px] bg-no-repeat bg-center bg-contain`} onClick={handleThemeSwitcher}></button>)}
-          {lightBg && (<button className={`${styles.icon_sun} w-[20px] h-[20px] bg-no-repeat bg-center bg-contain`} onClick={handleThemeSwitcher}></button>)}  
+          <button className={`${darkBg ? styles.icon_moon : styles.icon_sun} w-[20px] h-[20px] bg-no-repeat bg-center bg-contain`} onClick={handleThemeSwitcher}></button>
         </div>
       </div>
     </div>
