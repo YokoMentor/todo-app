90fffd1def2b09875c5af5a505259f57ccb78797
YokoMentor <91747328+YokoMentor@users.noreply.github.com>
2026-05-19 12:06:07 +0300

    Created a container for create new todo

--- app/page.tsx
+++ app/page.tsx
@@ -6,24 +6,28 @@ export default function Home() {
     const darkBgStyle = `${styles.bg_dark} flex flex-col min-h-screen justify-center items-center bg-bg-dark bg-no-repeat bg-contain`;
     const lightBgStyle = `${styles.bg_light} flex flex-col min-h-screen justify-center items-center bg-bg-light bg-no-repeat bg-contain`;
 
-    const [darkBg, setDarkBg] = useState(true);
+    const [darkTheme, setDarkTheme] = useState(true);
     const [themeSwitch, setThemeSwitch] = useState(true);
     
     function handleThemeSwitcher() {
     if (themeSwitch) {
-      setDarkBg(true);
+      setDarkTheme(true);
     } else {
-      setDarkBg(false);
+      setDarkTheme(false);
     }
     setThemeSwitch(!themeSwitch);
   }
 
   return (
-    <div className={`${darkBg ? darkBgStyle : lightBgStyle}`}>
+    <div className={`${darkTheme ? darkBgStyle : lightBgStyle}`}>
       <div className='flex flex-col justify-center items-center w-[328px]'>
-        <div className='flex justify-between items-baseline w-full'>
+        <div className='flex justify-between items-baseline w-full mb-6'>
           <h1 className='font-bold text-[28px] text-white tracking-[.32em]'>TODO</h1>
-          <button className={`${darkBg ? styles.icon_moon : styles.icon_sun} w-[20px] h-[20px] bg-no-repeat bg-center bg-contain`} onClick={handleThemeSwitcher}></button>
+          <button className={`${darkTheme ? styles.icon_moon : styles.icon_sun} w-[20px] h-[20px] bg-no-repeat bg-center bg-contain`} onClick={handleThemeSwitcher}></button>
+        </div>
+        <div className='flex items-center w-full bg-bg-container rounded-md h-[48px]'>
+          <div className='border border-disabled rounded-full w-[20px] h-[20px] ml-5 mr-3'></div>
+          <input type='text' placeholder='Create a new todo...' className='text-text-dark text-xs focus:outline-none focus:text-hover'/>
         </div>
       </div>
     </div>
